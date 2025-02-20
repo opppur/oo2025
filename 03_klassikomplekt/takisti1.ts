2b34ecaa48b81aca07da727ec58f86153e85d470
Jonas Oliveira <jonaso@tlu.ee>
2025-02-20 11:24:19 +0000

    20021324

--- 03_klassikomplekt/takisti1.ts
+++ 03_klassikomplekt/takisti1.ts
@@ -1,15 +1,20 @@
 class Resistor{
-    protected r: number;
-    protected g;
-    constructor(r: number, g:object){
-        this.r=r; 
-        this.g=g;
+    protected width: number;
+    protected height: number=30;
+    constructor(protected r: number, protected g, 
+         protected startx: number, protected endx: number, 
+          protected y: number){
+        this.width=this.endx-this.startx;
         this.draw();
     }
     draw(){
         this.g.beginPath();
-        this.g.rect(20, 10, 100, 30);
+        this.g.moveTo(this.startx, this.y);
+        this.g.lineTo(this.startx+this.width/4, this.y);
+        this.g.rect(this.startx+this.width/4, this.y-this.height/2, this.width/2, this.height);
+        this.g.moveTo(this.startx+this.width*3/4, this.y);
+        this.g.lineTo(this.endx, this.y);
         this.g.stroke();
-        this.g.fillText(""+this.r, 30, 20);
+        this.g.fillText(this.r+" Ω", this.startx+this.width/3, this.y+3);
     }
 }
